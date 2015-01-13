5776f2a28bd48735020fcd8b0c5f5eb17ca91498
won <wjin@nyit.edu>
2015-01-13 16:17:41 -0500

    added guardfile

---
+++ spec/requests/create_users_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+#   describe "CreateUsers" do
+#     it "creates new user session" do
+#       visit new_user_session_path
+#
+#       fill_in "name", "jenn"
+#       click_button "Submit"
+#     end
+# end
